19a7a09accd02706b68c07f1611a885893bc9793
bihellzin <gof2@cin.ufpe.br>
2020-01-22 05:03:24 -0300

    primeira questão revisada

--- questao_1.py
+++ questao_1.py
@@ -12,8 +12,8 @@ while bases:
         pass
 
     else:
-        # O pedaço do read possui 40% do tamanho do read completo
-        pedaco = read[:int(len(read) * 0.4)]
+        # O pedaço do read possui 20% do tamanho do read completo
+        pedaco = read[:int(len(read) * 0.2)]
 
 
         # Verificação para ver se o pedaço está presente na super string, caso não esteja, o read será colocado
@@ -22,16 +22,20 @@ while bases:
         if pedaco in super_string:
 
             # Percorro somente até onde importa
-            for j in range(int(len(super_string) * 0.7)):
+            for j in range(int(len(super_string) * 0.9)):
 
                 # Caso o pedaço do read esteja presente na super string, o número de letras já presentes na string é
                 # contabilizado e adiciono somente as letras restantes do read
                 if super_string[j:j + len(pedaco)] == pedaco:
                     qnt_caracteres_existente = len(super_string[j:j + len(pedaco)]) \
                                                + len(super_string[j + len(pedaco):])
-
-                    super_string += read[qnt_caracteres_existente:]
-                    break
+                    
+                    
+                    print(super_string[j: j + qnt_caracteres_existente])
+                    print()
+                    if super_string[j: j + qnt_caracteres_existente] == read[0: qnt_caracteres_existente]:
+                        super_string += read[qnt_caracteres_existente:]
+                        break
 
         else:
             bases.append(read)
